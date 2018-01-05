cdf85e5206d3e5718a7174a761050da72340cab9
linshuling <1611074393@qq.com>
2018-01-05 17:38:53 +0800

    localStorage

--- localstorage_test/js/index.js
+++ localstorage_test/js/index.js
@@ -2,7 +2,7 @@
 * @Author: linshuling
 * @Date:   2018-01-05 14:47:14
 * @Last Modified by:   linshuling
-* @Last Modified time: 2018-01-05 16:51:22
+* @Last Modified time: 2018-01-05 17:34:12
 */
 var page = {
     data : {},
@@ -12,13 +12,19 @@ var page = {
     onload : function(){
         this.bindEvent();
         this.unload();
-        this.renderBeforeInfo(window.localStorage["userInfo"]);
+        this.renderBeforeInfo(window.localStorage.getItem("userInfo"));
     },
     bindEvent : function(){
         var _this = this;
         //提交用户信息
         $(document).on('click', '.content .btn', function(){
             return;
+        });
+        //清除内容及localStorage内容
+        $(document).on('click', '.content .clear_btn', function(){
+            $('.user_id').val('');
+            $('.user_name').val('');
+            window.localStorage.clear();
         });
     },
     //使用localStorage存储本地数据(key,value)
@@ -35,14 +41,15 @@ var page = {
     },
     //获取localStrorage的数据信息渲染页面
     renderBeforeInfo : function(data){
-        var data = $.parseJSON(data);
+        // var data = $.parseJSON(data);
+        var data = JSON.parse(data);
         $('.user_id').val(data.userId);
         $('.user_name').val(data.userName);
     },
     //离开页面时存储信息
     unload :function(){
-        var _this = this;
-        window.onunload= function(){
+        var _this        = this;
+        window.onunload  = function(){
             var userInfo = {
                 userId      : $('.user_id').val(),
                 userName    : $('.user_name').val()
