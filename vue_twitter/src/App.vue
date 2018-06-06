738f0e90d3f94b8c46e9381c9c03f015c2414baa
linshuling <1611074393@qq.com>
2018-06-06 15:05:36 +0800

    这里有一个待完成dedemo,链接在App.vue里面

---
+++ vue_twitter/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+      <a href="https://juejin.im/entry/593003b4a0bb9f0058a95703">https://juejin.im/entry/593003b4a0bb9f0058a95703</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
